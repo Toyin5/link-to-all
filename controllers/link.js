4893f627665ce5c1c04bcaae67755e69438dc639
Toyin Muhammed <toyinmuhammed50@gmail.com>
2023-07-06 01:14:10 +0200

    feat: toggle public flag + delete link

--- controllers/link.js
+++ controllers/link.js
@@ -1,4 +1,4 @@
-import { body, validationResult } from "express-validator";
+import { body, validationResult, param } from "express-validator";
 import Links from "../models/links.js";
 import User from "../models/user.js";
 
@@ -33,11 +33,19 @@ export const addLink = async (req, res) => {
 };
 
 export const getLinks = async (req, res) => {
+  const errors = myValidationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const { authorised, id } = req.user;
+  const userExist = await User.findById(id);
+  if (!userExist) {
+    return res.status(404).json({ status: 404, error: "User not found" });
+  }
 
   if (!authorised) {
     const allLinks = await Links.find({
-      userId: id,
+      userId: userExist._id,
       public: true,
     });
     return res.status(200).json({
@@ -54,7 +62,7 @@ export const getLinks = async (req, res) => {
   }
 
   const allLinks = await Links.find({
-    userId: id,
+    userId: userExist._id,
   });
 
   return res.status(200).json({
@@ -70,6 +78,57 @@ export const getLinks = async (req, res) => {
   });
 };
 
+export const toggleStatus = async (req, res) => {
+  const errors = myValidationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const userExist = await User.findById(req.user.id);
+  if (!userExist) {
+    return res.status(404).json({ status: 404, error: "User not found" });
+  }
+  try {
+    const link = await Links.findOne({
+      _id: req.params.id,
+      userId: userExist._id,
+    });
+    link.public = !link.public;
+    await link.save();
+    res.status(200).json({
+      status: 200,
+      message: "Updated",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ status: 500, error: "Server error" });
+  }
+};
+
+export const deleteLink = async (req, res) => {
+  const errors = myValidationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const userExist = await User.findById(req.user.id);
+  if (!userExist) {
+    return res.status(404).json({ status: 404, error: "User not found" });
+  }
+  try {
+    const link = await Links.findOneAndDelete({
+      _id: req.params.id,
+      userId: userExist._id,
+    });
+    res.status(200).json({
+      status: 200,
+      message: "Deleted",
+      link,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ status: 500, error: "Server error" });
+  }
+};
+
 export const linkValidate = (method) => {
   switch (method) {
     case "addLink": {
@@ -79,6 +138,12 @@ export const linkValidate = (method) => {
       ];
     }
     case "getLinks": {
+      return [param("id", "Invalid id").exists().isUUID()];
+    }
+    case "toggleStatus": {
+      return [param("id", "Invalid id").exists().isUUID()];
+    }
+    case "deleteLink": {
       return [param("id", "Invalid id").exists().isUUID()];
     }
   }
--- routes/link.js
+++ routes/link.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { addLink, getLinks, linkValidate } from "../controllers/link.js";
+import {
+  addLink,
+  deleteLink,
+  getLinks,
+  linkValidate,
+  toggleStatus,
+} from "../controllers/link.js";
 import { checkHeader, getHeader } from "../middlewares/auth.js";
 
 export const linkRoute = express.Router();
 
 linkRoute.post("/link", [checkHeader, linkValidate("addLink")], addLink);
+linkRoute.patch(
+  "/link/:id",
+  [checkHeader, linkValidate("toggleStatus")],
+  toggleStatus
+);
+linkRoute.delete(
+  "/link/:id",
+  [checkHeader, linkValidate("deleteLink")],
+  deleteLink
+);
 linkRoute.get("/links/:id", [getHeader, linkValidate("getLinks")], getLinks);
